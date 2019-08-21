b3dbb41b01436f5e7c9d9283ad881399ac3c2e74
William Myllenberg <wm222au@student.lnu.se>
2019-08-21 16:02:03 +0200

    Added chance and odds to the incrementer to deny early stacking of incrementions

--- run.py
+++ run.py
@@ -1,25 +1,28 @@
+import math
 import random
 import cli.log
 from tabulate import tabulate
 
 
+def generate_number(range_start, range_end, odds):
+    number = 0
+    chance = random.uniform(0, 1)
+    if odds >= 1 or odds >= chance:
+        number = random.randint(math.floor(range_start), math.ceil(range_end))
+
+    return number
+
 def increment(start, end, increments, starting_point):
     numbers = []
     pot = end - start
-    number_distance = pot / increments  # Get distance between every increment, e.g. in a range of 20 with 10 increments, distance is 2
-    number_distance = number_distance if number_distance > 0 else 1
-
-    increment_number = number_distance
+    number_distance = float(pot) / increments  # Get distance between every increment, e.g. in a range of 20 with 10 increments, distance is 2
 
     for x in range(increments):
         last_number = start if x < 1 else numbers[x - 1][1]
-        current_number_distance = increment_number - number_distance
-        temp_actual_number_distance = number_distance + abs(current_number_distance)
-        actual_number_distance = temp_actual_number_distance if temp_actual_number_distance < pot else pot
-        temp_number = random.randint(0, actual_number_distance)
-        increment_number = 0 if temp_number < 0 else temp_number
-        pot = pot - increment_number
+        increment_number = generate_number(1, number_distance * 2, number_distance)
+        increment_number = pot if increment_number > pot else increment_number
         numbers.append([x + starting_point, last_number + increment_number])
+        pot = pot - increment_number
 
     return numbers
 
