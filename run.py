5b5deabd4712494cd9ff3b346a0cd44d4659146e
William Myllenberg <wm222au@student.lnu.se>
2019-08-21 13:40:29 +0200

    Added presentation and identifiers

--- run.py
+++ run.py
@@ -1,22 +1,36 @@
 import random
+import cli.log
+from tabulate import tabulate
 
-def generateCLIOptions():
-    print("CLI")
 
-def increment(start, end, increments, starting_point=0, use_decimals=None):
+@cli.log.LoggingApp
+def increment(app):
+    start = app.params.start
+    end = app.params.end
+    increments = app.params.increments
+    starting_point = app.params.starting_point
+
     numbers=[]
     number_distance = (end - start) / increments # Get distance between every increment, e.g. in a range of 20 with 10 increments, distance is 2
+    number_distance = number_distance if number_distance > 0 else 1
 
     increment_number = number_distance
 
     for x in range(increments):
-        last_number = start if x < 1 else numbers[x - 1]
+        last_number = start if x < 1 else numbers[x - 1][1]
         current_number_distance = increment_number - number_distance
         actual_number_distance = number_distance + abs(current_number_distance)
-        increment_number = random.randint(current_number_distance, actual_number_distance)
-        numbers.append(last_number + increment_number)
+        temp_number = random.randint(current_number_distance, actual_number_distance)
+        increment_number = 0 if temp_number < 0 else temp_number
+        numbers.append([x + starting_point, last_number + increment_number])
 
-    return numbers
+    print(tabulate(numbers, headers=["Id", "Number"]))
 
 
-print(increment(2, 20, 10))
+increment.add_param("start", help="", default=1, type=int)
+increment.add_param("end", help="", default=1, type=int)
+increment.add_param("increments", help="", default=1, type=int)
+increment.add_param("starting_point", help="", default=1, type=int)
+
+if __name__ == "__main__":
+    increment.run()
